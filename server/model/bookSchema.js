98c398977ec56ca571bb998e82d90be84f0aeb31
Peace Ishimwe <peaceishimwem@gmail.com>
2023-03-10 08:43:41 +0200

    prop

--- server/model/bookSchema.js
+++ server/model/bookSchema.js
@@ -1,22 +1,42 @@
+import { boolean } from "joi";
 import mongoose from "mongoose";
 import shortid from "shortid";
 
 const id  = shortid.generate()
 
 const signupSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    default: shortid.generate
-  },
-  bookname: {
-    type: String,
-    required: true,
-    unique: true,
-  },
   author: {
     type: String,
     required: true,
     unique: true,
+  },
+  country:{
+    type: String,
+    required: true,
+  },
+  imageLink:{
+    type: String,
+    required: true,
+  },
+  language:{
+    type: String,
+    required: true,
+  },
+  link:{
+    type: String,
+    required: true,
+  },
+  pages:{
+    type: Number,
+    required: true,
+  },
+  title:{
+    type: String,
+    required: true
+  },
+  year:{
+    type: Number,
+    required: true,
   }
 } , {
     collection: "books"
